997f4c65e1315fd992f1630b866320c03189d07d
Xiang Ma <marxwolfs@gmail.com>
2021-09-24 11:32:16 -0600

    ADD __add__ method

--- mymatrix.py
+++ mymatrix.py
@@ -8,7 +8,21 @@ class MyMatrix:
   def __mul__(self):
     pass
   
-  def __add_(self):
-    pass
+  def __add__(self, x, y):
+    """
+    x, y are MyMatrix object, the return is also MyMatrix object.
+    here, I assume __getitem__ is defined
+	here, I assume the init of MyMatrix using the nested list,
+	which is like this [[], []]
+    """
+    sums = []
+    for i in range(len(x)):
+    	row = []
+    	for j in range(len(y)):
+    		# row.append(x.index(i, j) + y.index(i, j))
+    		row.append(x[i][j] + y[i][j])
+    	sums.append(row)
+
+    return MyMatrix(sums)
   
  
